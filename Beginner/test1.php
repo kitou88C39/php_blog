852b2be866013c61e52a28a647c5bf879665b331
kitoumasaomi <kanfutrooper@gmail.com>
2025-11-07 14:24:02 +0900

    test1.phpを作成する

--- Beginner/test1.php
+++ Beginner/test1.php
@@ -6,4 +6,8 @@ $content = "PHPテストです";
 $post_at = "2025/11/05";
 $tag = ["PHP", "テスト", "ブログ"];
 
+echo $title; // セミコロンを追加
+echo $content; // セミコロンを追加
+echo $post_at; // セミコロンを追加
+print_r($tag);
 ?>
